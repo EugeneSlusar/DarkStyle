a4e9fbcafb68ae6d214ee043a4cee58d7ea67cf3
Eugene Slusar <eugene@slusar.ru>
2026-09-24 16:20:48 +0400

    Добавить ночной баннер с женскими руками

--- public_html/local/components/orgbox/home_slider/class.php
+++ public_html/local/components/orgbox/home_slider/class.php
@@ -86,7 +86,7 @@ class OrgBoxBaseShopHomeSliderComponent extends CBitrixComponent
         $demoImages = [
             'https://cdn-ru.bitrix24.ru/b21728636/ai/6cc/6cc0a82d4221ab6c976fc2fc3d588dbe/RP9k8c2e6BeSuz0oM9Nw5HwDTnQVZQk4.jpg',
             '/local/assets/site/process-application-v2.jpg',
-            '/local/assets/site/process-application-v2.jpg',
+            '/local/assets/site/window-tint-night-woman-wide.png',
         ];
         foreach ($items as $index => &$item) {
             $item['IMAGE'] = $demoImages[$index];
